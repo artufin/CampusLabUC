fc7024872d7507457c45f859cacf47bbe861e1cb
artufin <arturo.herreros@uc.cl>
2026-04-17 09:59:01 -0400

    feat: remove unused labels from OpportunityGraphic component

--- web/components/OpportunityGraphic.tsx
+++ web/components/OpportunityGraphic.tsx
@@ -2,33 +2,6 @@ import Image from "next/image";
 
 const GRAPHIC_SRC = "/assets/icons/desafios.svg";
 
-const LABELS = [
-  {
-    id: "forest",
-    title: "Investigacion Bosque",
-    icon: "/assets/icons/forest.svg",
-    className: "label-forest",
-  },
-  {
-    id: "tree",
-    title: "Tesis/Titulo Arbol",
-    icon: "/assets/icons/tree.svg",
-    className: "label-tree",
-  },
-  {
-    id: "plant",
-    title: "Practica Planta",
-    icon: "/assets/icons/plant.svg",
-    className: "label-plant",
-  },
-  {
-    id: "germination",
-    title: "Cursos Germinacion",
-    icon: "/assets/icons/germination.svg",
-    className: "label-germination",
-  },
-] as const;
-
 export function OpportunityGraphic() {
   return (
     <div className="opportunity-graphic">
